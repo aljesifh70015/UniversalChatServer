9b49f45fc9dcde9203eae156d96e8ea9d71754dd
aljesifh70015 <147716656+aljesifh70015@users.noreply.github.com>
2026-06-18 00:56:40 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,8 @@
+const mongoose = require("mongoose");
 
+const UserSchema = new mongoose.Schema({
+    uid: String,
+    username: String
+});
+
+module.exports = mongoose.model("User", UserSchema);
