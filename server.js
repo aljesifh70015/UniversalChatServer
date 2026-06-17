6f5b88a49b3c28152ac4fdb7ed81a490fbbf14a6
aljesifh70015 <147716656+aljesifh70015@users.noreply.github.com>
2026-06-17 17:01:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,46 +6,22 @@ const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
-    }
-});
-
-app.get("/", (req, res) => {
-    res.send("Universal Chat Server Running");
+    cors: { origin: "*" }
 });
 
 io.on("connection", (socket) => {
-    console.log("User connected:", socket.id);
-
-    socket.on("join_room", (roomId) => {
-        socket.join(roomId);
-        console.log("Joined room:", roomId);
-    });
 
     socket.on("send_message", (data) => {
-        socket.to(data.room).emit("receive_message", data);
+        socket.broadcast.emit("receive_message", data);
     });
 
-    socket.on("typing", (roomId) => {
-        socket.to(roomId).emit("typing");
-    });
-
-    socket.on("stop_typing", (roomId) => {
-        socket.to(roomId).emit("stop_typing");
-    });
-
-    socket.on("seen", (roomId) => {
-        socket.to(roomId).emit("seen");
-    });
-
-    socket.on("disconnect", () => {
-        console.log("User disconnected:", socket.id);
+    socket.on("delete_message", (messageId) => {
+        io.emit("delete_message", messageId);
     });
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log("Server running on port " + PORT);
+app.get("/", (req,res)=>{
+    res.send("Universal Chat Server Running");
 });
+
+server.listen(process.env.PORT || 3000);
