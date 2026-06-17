73bd5bc17b18074531093998d80fd2b253284502
aljesifh70015 <147716656+aljesifh70015@users.noreply.github.com>
2026-06-18 00:57:05 +0530

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -1,1 +1,10 @@
+const mongoose = require("mongoose");
 
+const MessageSchema = new mongoose.Schema({
+    roomId: String,
+    sender: String,
+    message: String,
+    timestamp: Number
+});
+
+module.exports = mongoose.model("Message", MessageSchema);
