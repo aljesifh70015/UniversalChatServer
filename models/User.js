87e7159b77d5a160543a33bd65fdb405a8957c67
aljesifh70015 <147716656+aljesifh70015@users.noreply.github.com>
2026-06-18 23:12:10 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -3,10 +3,14 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
     uid: {
         type: String,
+        required: true,
         unique: true
     },
 
-    username: String,
+    username: {
+        type: String,
+        required: true
+    },
 
     friends: {
         type: [String],
