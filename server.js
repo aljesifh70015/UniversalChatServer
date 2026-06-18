e446e60479b7a0484d00ed38e7ea726d4b5b03ed
aljesifh70015 <147716656+aljesifh70015@users.noreply.github.com>
2026-06-18 20:34:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const server = http.createServer(app);
 app.use(cors());
 app.use(express.json());
 
+// MongoDB
 mongoose.connect("mongodb+srv://aljesifhoque_db_user:67UF0MniHFtG98d2@cluster0.k3jzopc.mongodb.net/?appName=Cluster0")
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err));
@@ -19,11 +20,12 @@ const Message = require("./models/Message");
 
 const userLastSeen = {};
 
+// ================= API =================
+
 // Register
 app.post("/register", async (req, res) => {
     try {
         const { uid, username } = req.body;
-
         let user = await User.findOne({ uid });
 
         if (!user) {
@@ -33,7 +35,6 @@ app.post("/register", async (req, res) => {
                 friends: [],
                 friendRequests: []
             });
-
             await user.save();
         }
 
@@ -62,44 +63,59 @@ app.get("/user/:uid", async (req, res) => {
     }
 });
 
-// SEND FRIEND REQUEST
+// Send Friend Request
 app.post("/send_friend_request", async (req, res) => {
     try {
         const { myUid, friendUid } = req.body;
 
+        const me = await User.findOne({ uid: myUid });
         const friend = await User.findOne({ uid: friendUid });
 
-        if (!friend) {
+        if (!me || !friend) {
             return res.status(404).json({
                 message: "User not found"
             });
         }
 
-        if (!friend.friendRequests.includes(myUid)) {
-            friend.friendRequests.push(myUid);
-            await friend.save();
+        if (friend.friendRequests.includes(myUid)) {
+            return res.json({
+                message: "Already requested"
+            });
         }
 
-        res.json({ message: "Request sent" });
+        if (friend.friends.includes(myUid)) {
+            return res.json({
+                message: "Already friends"
+            });
+        }
+
+        friend.friendRequests.push(myUid);
+        await friend.save();
+
+        res.json({ message: "Request Sent" });
+
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
 });
 
-// GET REQUESTS
+// Get Friend Requests
 app.get("/friend_requests/:uid", async (req, res) => {
     try {
         const user = await User.findOne({ uid: req.params.uid });
 
-        if (!user) return res.json([]);
+        if (!user) {
+            return res.status(404).json([]);
+        }
 
         res.json(user.friendRequests);
+
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
 });
 
-// ACCEPT REQUEST
+// Accept Friend Request
 app.post("/accept_friend_request", async (req, res) => {
     try {
         const { myUid, friendUid } = req.body;
@@ -113,22 +129,59 @@ app.post("/accept_friend_request", async (req, res) => {
             });
         }
 
-        me.friendRequests =
-            me.friendRequests.filter(uid => uid !== friendUid);
+        me.friendRequests = me.friendRequests.filter(
+            uid => uid !== friendUid
+        );
 
-        if (!me.friends.includes(friendUid))
+        if (!me.friends.includes(friendUid)) {
             me.friends.push(friendUid);
+        }
 
-        if (!friend.friends.includes(myUid))
+        if (!friend.friends.includes(myUid)) {
             friend.friends.push(myUid);
+        }
 
         await me.save();
         await friend.save();
 
-        res.json({ message: "Friend added" });
+        res.json({ message: "Friend Added" });
+
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
+});
+
+// Reject Friend Request
+app.post("/reject_friend_request", async (req, res) => {
+    try {
+        const { myUid, friendUid } = req.body;
+
+        const me = await User.findOne({ uid: myUid });
+
+        if (!me) {
+            return res.status(404).json({
+                message: "User not found"
+            });
+        }
+
+        me.friendRequests = me.friendRequests.filter(
+            uid => uid !== friendUid
+        );
+
+        await me.save();
+
+        res.json({ message: "Request Rejected" });
+
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+// OLD Add Friend (compatibility)
+app.post("/add_friend", async (req, res) => {
+    res.json({
+        message: "Use send_friend_request"
+    });
 });
 
 // Message history
@@ -144,26 +197,30 @@ app.get("/messages/:roomId", async (req, res) => {
     }
 });
 
-// Friends list
+// Friends
 app.get("/friends/:uid", async (req, res) => {
     try {
-        const user = await User.findOne({
-            uid: req.params.uid
-        });
+        const user = await User.findOne({ uid: req.params.uid });
 
-        if (!user) return res.json([]);
+        if (!user) {
+            return res.json([]);
+        }
 
         res.json(user.friends);
+
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
 });
+
+// ================= SOCKET =================
 
 const io = new Server(server, {
     cors: { origin: "*" }
 });
 
 io.on("connection", (socket) => {
+    console.log("User connected:", socket.id);
 
     socket.on("join_room", (roomId) => {
         socket.join(roomId);
@@ -171,7 +228,7 @@ io.on("connection", (socket) => {
 
     socket.on("user_online", (uid) => {
         io.emit("user_status", {
-            uid,
+            uid: uid,
             status: "Online"
         });
     });
@@ -199,30 +256,40 @@ io.on("connection", (socket) => {
         userLastSeen[uid] = lastSeen;
 
         io.emit("user_status", {
-            uid,
+            uid: uid,
             status: lastSeen
         });
     });
 
     socket.on("send_message", async (data) => {
-        const msg = new Message({
-            roomId: data.roomId,
-            sender: data.sender,
-            message: data.message,
-            timestamp: Date.now()
-        });
+        try {
+            const msg = new Message({
+                roomId: data.roomId,
+                sender: data.sender,
+                message: data.message,
+                timestamp: Date.now()
+            });
 
-        await msg.save();
+            await msg.save();
 
-        io.to(data.roomId).emit("receive_message", data);
-        io.to(data.roomId).emit("message_delivered");
+            io.to(data.roomId).emit("receive_message", data);
+            io.to(data.roomId).emit("message_delivered");
+
+        } catch (err) {
+            console.log(err);
+        }
     });
 
     socket.on("message_seen", (roomId) => {
         io.to(roomId).emit("message_seen");
     });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected");
+    });
 });
 
+// Root
 app.get("/", (req, res) => {
     res.send("Chat server running");
 });
