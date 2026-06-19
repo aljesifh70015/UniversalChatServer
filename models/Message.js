17841beda47d000d539e5b72aadfc12ee957c8af
aljesifh70015 <147716656+aljesifh70015@users.noreply.github.com>
2026-06-19 16:31:39 +0530

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -1,10 +1,46 @@
 const mongoose = require("mongoose");
 
 const MessageSchema = new mongoose.Schema({
-    roomId: String,
-    sender: String,
-    message: String,
-    timestamp: Number
+    messageId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    roomId: {
+        type: String,
+        required: true
+    },
+
+    sender: {
+        type: String,
+        required: true
+    },
+
+    message: {
+        type: String,
+        required: true
+    },
+
+    timestamp: {
+        type: Number,
+        required: true
+    },
+
+    expiresAt: {
+        type: Number,
+        default: 0
+    },
+
+    deletedFor: {
+        type: [String],
+        default: []
+    },
+
+    edited: {
+        type: Boolean,
+        default: false
+    }
 });
 
 module.exports = mongoose.model("Message", MessageSchema);
