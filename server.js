6cc01e05761b4f52e707dcf60bf1639b206bf826
aljesifh70015 <147716656+aljesifh70015@users.noreply.github.com>
2026-06-21 00:46:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -407,6 +407,8 @@ app.get("/", (req, res) => {
     res.send("Chat server running");
 });
 
-server.listen(3000, () => {
-    console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
 });
