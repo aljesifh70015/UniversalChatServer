9edfe7ee5166a9dd8521c4f221952d4dfc8ebdf1
aljesifh70015 <147716656+aljesifh70015@users.noreply.github.com>
2026-06-20 20:11:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -263,9 +263,26 @@ io.on("connection", (socket) => {
         socket.join(roomId);
     });
 
-    socket.on("user_online", (uid) => {
-        io.emit("user_status", { uid, status: "Online" });
-    });
+    const onlineUsers = {};
+
+socket.on("user_online", (uid) => {
+    onlineUsers[uid] = socket.id;
+    io.emit("user_status", { uid, status: "Online" });
+});
+
+socket.on("disconnect", () => {
+    for (const uid in onlineUsers) {
+        if (onlineUsers[uid] === socket.id) {
+            delete onlineUsers[uid];
+
+            io.emit("user_status", {
+                uid,
+                status: "Offline"
+            });
+            break;
+        }
+    }
+});
 
     socket.on("typing", (data) => {
         socket.to(data.roomId).emit("typing");
