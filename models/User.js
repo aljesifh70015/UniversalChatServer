e038b5b5a1bcaa7047158287c2e7da67a23d61c6
aljesifh70015 <147716656+aljesifh70015@users.noreply.github.com>
2026-06-30 00:35:19 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,61 +1,72 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    uid: {
-        type: String,
-        required: true,
-        unique: true
-    },
+uid: {
+type: String,
+required: true,
+unique: true
+},
 
-    username: {
-        type: String,
-        required: true
-    },
+username: {
+    type: String,
+    required: true
+},
 
-    email: {
-        type: String,
-        default: ""
-    },
+email: {
+    type: String,
+    default: ""
+},
 
-    password: {
-        type: String,
-        default: ""
-    },
+password: {
+    type: String,
+    default: ""
+},
 
-    verified: {
-        type: Boolean,
-        default: false
-    },
+verified: {
+    type: Boolean,
+    default: false
+},
 
-    loggedInDevice: {
-        type: String,
-        default: ""
-    },
+loggedInDevice: {
+    type: String,
+    default: ""
+},
 
-    friends: {
-        type: [String],
-        default: []
-    },
+friends: {
+    type: [String],
+    default: []
+},
 
-    friendRequests: {
-        type: [String],
-        default: []
-    },
+friendRequests: {
+    type: [String],
+    default: []
+},
 
-    lastSeen: {
-        type: String,
-        default: ""
-    },
+lastSeen: {
+    type: String,
+    default: ""
+},
 
-    fcmToken: {
-        type: String,
-        default: ""
-    },
+fcmToken: {
+    type: String,
+    default: ""
+},
 
-    profilePic: {
-        type: String,
-        default: ""
-    }
+profilePic: {
+    type: String,
+    default: ""
+},
+
+name: {
+    type: String,
+    default: ""
+},
+
+bio: {
+    type: String,
+    default: ""
+}
+
 });
 
 module.exports = mongoose.model("User", UserSchema);
